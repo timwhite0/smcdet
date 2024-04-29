48379f9f703a55aab192b4560417f227c06893d9
Tim White <twhit@umich.edu>
2024-04-28 22:09:17 -0400

    Add axes/angles flag to sampler

--- smc/sampler.py
+++ smc/sampler.py
@@ -56,7 +56,7 @@ class SMCsampler(object):
         self.kernel_fluors_low = self.prior.fluor_prior.lb * torch.ones(1)
         self.kernel_fluors_high = self.prior.fluor_prior.ub * torch.ones(1)
         
-        self.kernel_locs_stdev = 0.25 * torch.ones(1)
+        self.kernel_locs_stdev = 1 * torch.ones(1)
         self.kernel_locs_low = torch.zeros(1) - torch.tensor(self.prior.pad)
         self.kernel_locs_high = torch.tensor(self.tile_attr.img_height) + torch.tensor(self.prior.pad)
         
@@ -142,7 +142,7 @@ class SMCsampler(object):
         self.temperature = self.temperature + delta
     
     
-    def resample(self):
+    def resample(self, axes_angles = False):
         for block_num in range(self.num_blocks):
             resampled_index = self.weights_intrablock[:,:,block_num,:].flatten(0,1).multinomial(self.catalogs_per_block,
                                                                                                 replacement = True).unflatten(0, (self.num_tiles_h, self.num_tiles_w))
@@ -155,67 +155,87 @@ class SMCsampler(object):
                 for w in range(self.num_tiles_w):
                     f = self.fluors[h,w,lower:upper,:]
                     l = self.locs[h,w,lower:upper,:,:]
-                    ax = self.axes[h,w,lower:upper,:,:]
-                    an = self.angles[h,w,lower:upper,:]
                     self.fluors[h,w,lower:upper,:] = f[resampled_index[h,w,:],:]
                     self.locs[h,w,lower:upper,:,:] = l[resampled_index[h,w,:],:,:]
-                    self.axes[h,w,lower:upper,:,:] = ax[resampled_index[h,w,:],:,:]
-                    self.angles[h,w,lower:upper,:] = an[resampled_index[h,w,:],:]
+                    if axes_angles == True:
+                        ax = self.axes[h,w,lower:upper,:,:]
+                        an = self.angles[h,w,lower:upper,:]
+                        self.axes[h,w,lower:upper,:,:] = ax[resampled_index[h,w,:],:,:]
+                        self.angles[h,w,lower:upper,:] = an[resampled_index[h,w,:],:]
                     
             self.weights_intrablock[:,:,block_num,:] = (1/self.catalogs_per_block) * torch.ones(self.num_tiles_h, self.num_tiles_w, self.catalogs_per_block)
             self.weights_interblock[:,:,lower:upper] = (self.weights_interblock[:,:,lower:upper].sum(2)/self.catalogs_per_block).unsqueeze(2).repeat(1, 1, self.catalogs_per_block)
     
     
-    def MH(self, num_iters):
-        
+    def MH(self, num_iters, axes_angles = False):
         count_indicator = torch.arange(1, self.num_blocks).unsqueeze(0) <= self.counts.unsqueeze(3)
         
         fluors_prev = self.fluors
         locs_prev = self.locs
-        axes_prev = self.axes
-        angles_prev = self.angles
+        if axes_angles == True:
+            axes_prev = self.axes
+            angles_prev = self.angles
         
         for iter in range(num_iters):
             fluors_proposed = (TruncatedDiagonalMVN(fluors_prev, self.kernel_fluors_stdev,
                                                     self.kernel_fluors_low, self.kernel_fluors_high).sample().nan_to_num()) * count_indicator
             locs_proposed = TruncatedDiagonalMVN(locs_prev, self.kernel_locs_stdev,
                                                  self.kernel_locs_low, self.kernel_locs_high).sample() * count_indicator.unsqueeze(4)
-            axes_proposed = (TruncatedDiagonalMVN(axes_prev, self.kernel_axes_stdev,
-                                                  self.kernel_axes_low, self.kernel_axes_high).sample().nan_to_num()) * count_indicator.unsqueeze(4)
-            angles_unconstrained = TruncatedDiagonalMVN(angles_prev, self.kernel_angles_stdev,
-                                                        self.kernel_angles_low, self.kernel_angles_high).sample()
-            angles_proposed = (angles_unconstrained + torch.pi * (angles_unconstrained <= 0.) - torch.pi * (angles_unconstrained >= torch.pi)) * count_indicator
+            if axes_angles == True:
+                axes_proposed = (TruncatedDiagonalMVN(axes_prev, self.kernel_axes_stdev,
+                                                    self.kernel_axes_low, self.kernel_axes_high).sample().nan_to_num()) * count_indicator.unsqueeze(4)
+                angles_unconstrained = TruncatedDiagonalMVN(angles_prev, self.kernel_angles_stdev,
+                                                            self.kernel_angles_low, self.kernel_angles_high).sample()
+                angles_proposed = (angles_unconstrained + torch.pi * (angles_unconstrained <= 0.) - torch.pi * (angles_unconstrained >= torch.pi)) * count_indicator
             
-            log_numerator = self.log_target(self.counts, fluors_proposed, locs_proposed, axes_proposed, angles_proposed, self.temperature_prev)
-            log_numerator += (TruncatedDiagonalMVN(fluors_proposed, self.kernel_fluors_stdev,
+            if axes_angles == False:
+                log_numerator = self.log_target(self.counts, fluors_proposed, locs_proposed, self.axes, self.angles, self.temperature_prev)
+                log_numerator += (TruncatedDiagonalMVN(fluors_proposed, self.kernel_fluors_stdev,
                                                    self.kernel_fluors_low,
                                                    self.kernel_fluors_high).log_prob(fluors_prev +
                                                                                      self.prior.fluor_prior.base_dist.mean * (fluors_prev==0.)) * count_indicator).nan_to_num().sum(3)
-            log_numerator += (TruncatedDiagonalMVN(locs_proposed, self.kernel_locs_stdev,
-                                                   self.kernel_locs_low, self.kernel_locs_high).log_prob(locs_prev) * count_indicator.unsqueeze(4)).sum([3,4])
-            log_numerator += (TruncatedDiagonalMVN(axes_proposed, self.kernel_axes_stdev,
-                                                   self.kernel_axes_low,
-                                                   self.kernel_axes_high).log_prob(axes_prev +
-                                                                                   self.prior.axis_prior.mean.unique() * (axes_prev==0.)) * count_indicator.unsqueeze(4)).nan_to_num().sum([3,4])
-            log_numerator += TruncatedDiagonalMVN(angles_proposed, self.kernel_angles_stdev,
-                                                  self.kernel_angles_low,
-                                                  self.kernel_angles_high).log_prob(angles_prev).sum(3)
+                log_numerator += (TruncatedDiagonalMVN(locs_proposed, self.kernel_locs_stdev,
+                                                    self.kernel_locs_low, self.kernel_locs_high).log_prob(locs_prev) * count_indicator.unsqueeze(4)).sum([3,4])
+            elif axes_angles == True:
+                log_numerator = self.log_target(self.counts, fluors_proposed, locs_proposed, axes_proposed, angles_proposed, self.temperature_prev)
+                log_numerator += (TruncatedDiagonalMVN(fluors_proposed, self.kernel_fluors_stdev,
+                                                    self.kernel_fluors_low,
+                                                    self.kernel_fluors_high).log_prob(fluors_prev +
+                                                                                        self.prior.fluor_prior.base_dist.mean * (fluors_prev==0.)) * count_indicator).nan_to_num().sum(3)
+                log_numerator += (TruncatedDiagonalMVN(locs_proposed, self.kernel_locs_stdev,
+                                                    self.kernel_locs_low, self.kernel_locs_high).log_prob(locs_prev) * count_indicator.unsqueeze(4)).sum([3,4])
+                log_numerator += (TruncatedDiagonalMVN(axes_proposed, self.kernel_axes_stdev,
+                                                    self.kernel_axes_low,
+                                                    self.kernel_axes_high).log_prob(axes_prev +
+                                                                                    self.prior.axis_prior.mean.unique() * (axes_prev==0.)) * count_indicator.unsqueeze(4)).nan_to_num().sum([3,4])
+                log_numerator += TruncatedDiagonalMVN(angles_proposed, self.kernel_angles_stdev,
+                                                    self.kernel_angles_low,
+                                                    self.kernel_angles_high).log_prob(angles_prev).sum(3)
 
             if iter == 0:
-                log_denominator = self.log_target(self.counts, fluors_prev, locs_prev, axes_prev, angles_prev, self.temperature_prev)
-                log_denominator += (TruncatedDiagonalMVN(fluors_prev, self.kernel_fluors_stdev,
+                if axes_angles == False:
+                    log_denominator = self.log_target(self.counts, fluors_prev, locs_prev, self.axes, self.angles, self.temperature_prev)
+                    log_denominator += (TruncatedDiagonalMVN(fluors_prev, self.kernel_fluors_stdev,
                                                          self.kernel_fluors_low,
                                                          self.kernel_fluors_high).log_prob(fluors_proposed +
                                                                                            self.prior.fluor_prior.base_dist.mean * (fluors_proposed==0.)) * count_indicator).nan_to_num().sum(3)
-                log_denominator += (TruncatedDiagonalMVN(locs_prev, self.kernel_locs_stdev,
-                                                         self.kernel_locs_low, self.kernel_locs_high).log_prob(locs_proposed) * count_indicator.unsqueeze(4)).sum([3,4])
-                log_denominator += (TruncatedDiagonalMVN(axes_prev, self.kernel_axes_stdev,
-                                                         self.kernel_axes_low,
-                                                         self.kernel_axes_high).log_prob(axes_proposed +
-                                                                                         self.prior.axis_prior.mean.unique() * (axes_proposed==0.)) * count_indicator.unsqueeze(4)).nan_to_num().sum([3,4])
-                log_denominator += TruncatedDiagonalMVN(angles_prev, self.kernel_angles_stdev,
-                                                        self.kernel_angles_low,
-                                                        self.kernel_angles_high).log_prob(angles_proposed).sum(3)
+                    log_denominator += (TruncatedDiagonalMVN(locs_prev, self.kernel_locs_stdev,
+                                                            self.kernel_locs_low, self.kernel_locs_high).log_prob(locs_proposed) * count_indicator.unsqueeze(4)).sum([3,4])
+                elif axes_angles == True:
+                    log_denominator = self.log_target(self.counts, fluors_prev, locs_prev, axes_prev, angles_prev, self.temperature_prev)
+                    log_denominator += (TruncatedDiagonalMVN(fluors_prev, self.kernel_fluors_stdev,
+                                                            self.kernel_fluors_low,
+                                                            self.kernel_fluors_high).log_prob(fluors_proposed +
+                                                                                            self.prior.fluor_prior.base_dist.mean * (fluors_proposed==0.)) * count_indicator).nan_to_num().sum(3)
+                    log_denominator += (TruncatedDiagonalMVN(locs_prev, self.kernel_locs_stdev,
+                                                            self.kernel_locs_low, self.kernel_locs_high).log_prob(locs_proposed) * count_indicator.unsqueeze(4)).sum([3,4])
+                    log_denominator += (TruncatedDiagonalMVN(axes_prev, self.kernel_axes_stdev,
+                                                            self.kernel_axes_low,
+                                                            self.kernel_axes_high).log_prob(axes_proposed +
+                                                                                            self.prior.axis_prior.mean.unique() * (axes_proposed==0.)) * count_indicator.unsqueeze(4)).nan_to_num().sum([3,4])
+                    log_denominator += TruncatedDiagonalMVN(angles_prev, self.kernel_angles_stdev,
+                                                            self.kernel_angles_low,
+                                                            self.kernel_angles_high).log_prob(angles_proposed).sum(3)
             
             log_alpha = (log_numerator - log_denominator).clamp(max = 0)
             log_p = Uniform(torch.zeros(self.num_tiles_h, self.num_tiles_w, self.num_catalogs),
@@ -224,16 +244,22 @@ class SMCsampler(object):
             
             fluors_new = fluors_proposed * (accept).unsqueeze(3) + fluors_prev * (~accept).unsqueeze(3)
             locs_new = locs_proposed * (accept).view(self.num_tiles_h, self.num_tiles_w, -1, 1, 1) + locs_prev * (~accept).view(self.num_tiles_h, self.num_tiles_w, -1, 1, 1)
-            axes_new = axes_proposed * (accept).view(self.num_tiles_h, self.num_tiles_w, -1, 1, 1) + axes_prev * (~accept).view(self.num_tiles_h, self.num_tiles_w, -1, 1, 1)
-            angles_new = angles_proposed * (accept).unsqueeze(3) + angles_prev * (~accept).unsqueeze(3)
+            if axes_angles == True:
+                axes_new = axes_proposed * (accept).view(self.num_tiles_h, self.num_tiles_w, -1, 1, 1) + axes_prev * (~accept).view(self.num_tiles_h, self.num_tiles_w, -1, 1, 1)
+                angles_new = angles_proposed * (accept).unsqueeze(3) + angles_prev * (~accept).unsqueeze(3)
 
             # Cache log_denominator for next iteration
             log_denominator = log_numerator * (accept) + log_denominator * (~accept)
             
             fluors_prev = fluors_new
             locs_prev = locs_new
-            axes_prev = axes_new
-            angles_prev = angles_new
+            if axes_angles == True:
+                axes_prev = axes_new
+                angles_prev = angles_new
+        
+        if axes_angles == False:
+            axes_new = self.axes
+            angles_new = self.angles
         
         return [fluors_new, locs_new, axes_new, angles_new]
     
