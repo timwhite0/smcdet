57f6ad640542ee2b77aed2452c029f21297c80fe
Tim White <twhit@umich.edu>
2024-06-30 20:24:30 -0400

    Refactor loglikelihood, move to images.py, and rename ImageAttributes -> ImageModel

--- smc/images.py
+++ smc/images.py
@@ -1,16 +1,16 @@
 import torch
 from torch.distributions import Poisson
 
-class ImageAttributes(object):
+class ImageModel(object):
     def __init__(self,
-                 img_dim,
+                 image_dim,
                  psf_stdev,
                  background):
-        self.img_dim = img_dim
+        self.image_dim = image_dim
         
-        psf_marginal = 1 + torch.arange(self.img_dim, dtype = torch.float32)
-        self.psf_marginal_h = psf_marginal.view(1, self.img_dim, 1, 1)
-        self.psf_marginal_w = psf_marginal.view(1, 1, self.img_dim, 1)
+        psf_marginal = 1 + torch.arange(self.image_dim, dtype = torch.float32)
+        self.psf_marginal_h = psf_marginal.view(1, self.image_dim, 1, 1)
+        self.psf_marginal_w = psf_marginal.view(1, 1, self.image_dim, 1)
         
         self.psf_stdev = psf_stdev
         
@@ -28,11 +28,20 @@ class ImageAttributes(object):
     
     def generate(self, prior, num = 1):
         catalogs = prior.sample(num_catalogs = num)
-        counts, locs, fluxes = catalogs
+        counts, locs, features = catalogs
         
-        source_intensities = (fluxes.unsqueeze(1).unsqueeze(2) * self.psf(locs)).sum(3)
-        total_intensities = source_intensities + self.background
+        psf = self.psf(locs)
+        rate = (psf * features.unsqueeze(-2).unsqueeze(-3)).sum(3) + self.background
+        images = Poisson(rate).sample()
         
-        images = Poisson(total_intensities).sample()
-        
-        return [counts, locs, fluxes, total_intensities, images]
+        return [counts, locs, features, images]
+
+
+    # first two dims of tiled_image should be num_tiles_per_side x num_tiles_per_side
+    def loglikelihood(self, tiled_image, locs, features):
+        psf = self.psf(locs)
+        rate = (psf * features.unsqueeze(-2).unsqueeze(-3)).sum(5) + self.background
+        rate = rate.permute((0, 1, 3, 4, 2))
+        loglik = Poisson(rate).log_prob(tiled_image.unsqueeze(4)).sum([2,3])
+
+        return loglik
