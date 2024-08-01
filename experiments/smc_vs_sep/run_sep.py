a7772e9ff26359e9bfabc54843687f671d60827b
Tim White <twhit@umich.edu>
2024-08-01 01:34:59 -0400

    Padding and pruning for SEP

--- experiments/smc_vs_sep/run_sep.py
+++ experiments/smc_vs_sep/run_sep.py
@@ -10,6 +10,8 @@ torch.set_default_device(device)
 
 import time
 import sep
+
+from torch.nn.functional import pad
 ##############################################
 
 ##############################################
@@ -24,6 +26,9 @@ num_images = images.shape[0]
 image_height = images.shape[1]
 image_width = images.shape[2]
 background = 100000
+padding = 2
+padded_images = pad(images, (padding, padding, padding, padding), mode = 'reflect')
+max_detections = 100
 ##############################################
 
 ##############################################
@@ -31,10 +36,10 @@ background = 100000
 
 print('Starting grid search...\n')
 
-thresh = torch.linspace(start = 500, end = 2000, steps = 4)
+thresh = torch.linspace(start = 1000, end = 2000, steps = 3)
 minarea = torch.linspace(start = 1, end = 5, steps = 3)
 deblend_cont = torch.logspace(start = -4, end = -2, steps = 3)
-clean_param = torch.logspace(start = -3, end = 1, steps = 3)
+clean_param = torch.logspace(start = -1, end = 1, steps = 3)
 
 sep_estimated_count = torch.zeros(thresh.shape[0], minarea.shape[0],
                                   deblend_cont.shape[0],
@@ -52,15 +57,27 @@ for t in range(thresh.shape[0]):
                 print(f'clean_param = {clean_param[c]}\n')
                 
                 for i in range(num_images):
-                    detected_sources = sep.extract((images[i] - background).cpu().numpy(),
-                                                    thresh = thresh[t], minarea = minarea[m],
-                                                    deblend_cont = deblend_cont[d],
-                                                    deblend_nthresh = 2000,
-                                                    filter_kernel = None,
-                                                    clean = True, clean_param = clean_param[c])
-                    sep_estimated_count[t, m, d, c, i] = len(detected_sources)
+                    sep_results = sep.extract((padded_images[i] - background).cpu().numpy(),
+                                              thresh = thresh[t], minarea = minarea[m],
+                                              deblend_cont = deblend_cont[d],
+                                              deblend_nthresh = 2000,
+                                              filter_kernel = None,
+                                              clean = True, clean_param = clean_param[c])
+                    
+                    count = len(sep_results)
+                    locs = torch.zeros(max_detections, 2)
+                    locs[:count,0] = torch.from_numpy(sep_results['y']) - padding + 0.5  # match SMC locs convention
+                    locs[:count,1] = torch.from_numpy(sep_results['x']) - padding + 0.5  # match SMC locs convention
+                    
+                    in_bounds = torch.all(
+                        torch.logical_and(locs > 0,
+                                          locs < torch.tensor((image_height, image_width))),
+                        dim = -1
+                    )
+                    sep_estimated_count[t, m, d, c, i] = in_bounds.sum(-1).float()
 
-                sep_mse[t,m,d,c] = ((sep_estimated_count[t, m, d, c,:] - true_counts)**2).mean()
+                sep_mse[t,m,d,c] = ((sep_estimated_count[t, m, d, c,:] - true_counts) ** 2).mean()
+                print(f'mse = {sep_mse[t,m,d,c]}\n\n')
 
 for t in range(thresh.shape[0]):
     for m in range(minarea.shape[0]):
@@ -84,7 +101,6 @@ print(f'clean_param = {clean_param_best}\n')
 
 print('Running SEP...\n')
 
-max_detections = 100
 runtime = torch.zeros(num_images)
 counts = torch.zeros(num_images)
 locs = torch.zeros(num_images, max_detections, 2)
@@ -93,7 +109,7 @@ fluxes = torch.zeros(num_images, max_detections)
 for i in range(num_images):
     start = time.perf_counter()
     
-    sep_results = sep.extract((images[i] - background).cpu().numpy(),
+    sep_results = sep.extract((padded_images[i] - background).cpu().numpy(),
                               thresh = thresh_best, minarea = minarea_best,
                               deblend_cont = deblend_cont_best,
                               deblend_nthresh = 2000, filter_kernel = None,
@@ -104,13 +120,32 @@ for i in range(num_images):
     runtime[i] = end - start
     counts[i] = len(sep_results)
     count = counts[i].int().item()
-    locs[i,:count,0] = torch.from_numpy(sep_results['x'])
-    locs[i,:count,1] = torch.from_numpy(sep_results['y'])
+    locs[i,:count,0] = torch.from_numpy(sep_results['y']) - padding + 0.5  # match SMC locs convention
+    locs[i,:count,1] = torch.from_numpy(sep_results['x']) - padding + 0.5  # match SMC locs convention
     fluxes[i,:count] = torch.from_numpy(sep_results['flux'])
 
 # remove unnecessary trailing zeros
 locs = locs[:,:counts.max().int().item(),:]
 fluxes = fluxes[:,:counts.max().int().item()]
+
+# prune detections a la Aggregate.prune
+in_bounds = torch.all(
+    torch.logical_and(locs > 0,
+                      locs < torch.tensor((image_height, image_width))),
+    dim = -1
+)
+
+counts = in_bounds.sum(-1).float()
+
+locs = in_bounds.unsqueeze(-1) * locs
+locs_mask = (locs != 0).int()
+locs_index = torch.sort(locs_mask, dim = 1, descending = True)[1]
+locs = torch.gather(locs, dim = 1, index = locs_index)
+
+fluxes = in_bounds * fluxes
+fluxes_mask = (fluxes != 0).int()
+fluxes_index = torch.sort(fluxes_mask, dim = 1, descending = True)[1]
+fluxes = torch.gather(fluxes, dim = 1, index = fluxes_index)
 ##############################################
 
 ##############################################
