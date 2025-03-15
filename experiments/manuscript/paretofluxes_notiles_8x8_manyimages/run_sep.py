99ffb2120e6385d60da91eb9f1a86c1b85ed5a12
Tim White <twhit@umich.edu>
2025-03-15 12:20:08 -0400

    Updated SEP script for Pareto, no tiles, many 8x8 images

--- experiments/manuscript/paretofluxes_notiles_8x8_manyimages/run_sep.py
+++ experiments/manuscript/paretofluxes_notiles_8x8_manyimages/run_sep.py
@@ -24,17 +24,18 @@ torch.set_default_device(device)
 # LOAD IN IMAGES AND CATALOGS
 
 images = torch.load("data/images.pt").to(device)
-true_counts = torch.load("data/true_counts.pt").to(device)
-true_locs = torch.load("data/true_locs.pt").to(device)
-true_fluxes = torch.load("data/true_fluxes.pt").to(device)
+unpruned_counts = torch.load("data/unpruned_counts.pt").to(device)
+pruned_counts = torch.load("data/pruned_counts.pt").to(device)
+unpruned_fluxes = torch.load("data/unpruned_fluxes.pt").to(device)
+pruned_fluxes = torch.load("data/pruned_fluxes.pt").to(device)
 
 num_images = images.shape[0]
 image_height = images.shape[1]
 image_width = images.shape[2]
 background = 200
-padding = 1  # true padding is 0.3, but padding needs to be an int here
+padding = 0  # true padding is < 1, but padding needs to be an int here
 padded_images = pad(images, (padding, padding, padding, padding), mode="reflect")
-max_detections = 100
+max_detections = 50
 ##############################################
 
 ##############################################
@@ -42,12 +43,12 @@ max_detections = 100
 
 print("Starting grid search...\n")
 
-thresh = torch.linspace(start=30, end=75, steps=10)
+thresh = torch.linspace(start=20, end=65, steps=10)
 minarea = torch.linspace(start=1, end=3, steps=3)
-deblend_cont = torch.logspace(start=-4, end=-2, steps=3)
-clean_param = torch.logspace(start=-1, end=1, steps=3)
+deblend_cont = torch.logspace(start=-5, end=-3, steps=3)
+clean_param = torch.logspace(start=0, end=2, steps=3)
 
-sep_estimated_count = torch.zeros(
+pruned_count = torch.zeros(
     thresh.shape[0],
     minarea.shape[0],
     deblend_cont.shape[0],
@@ -79,12 +80,12 @@ for t in range(thresh.shape[0]):
                         clean_param=clean_param[c],
                     )
 
-                    count = len(sep_results)
+                    unpruned_count = len(sep_results)
                     locs = torch.zeros(max_detections, 2)
-                    locs[:count, 0] = (
+                    locs[:unpruned_count, 0] = (
                         torch.from_numpy(sep_results["y"]) - padding + 0.5
                     )  # match SMC locs convention
-                    locs[:count, 1] = (
+                    locs[:unpruned_count, 1] = (
                         torch.from_numpy(sep_results["x"]) - padding + 0.5
                     )  # match SMC locs convention
 
@@ -94,10 +95,10 @@ for t in range(thresh.shape[0]):
                         ),
                         dim=-1,
                     )
-                    sep_estimated_count[t, m, d, c, i] = in_bounds.sum(-1).float()
+                    pruned_count[t, m, d, c, i] = in_bounds.sum(-1).float()
 
                 sep_mse[t, m, d, c] = (
-                    (sep_estimated_count[t, m, d, c, :] - true_counts) ** 2
+                    (pruned_count[t, m, d, c, :] - pruned_counts) ** 2
                 ).mean()
                 print(f"mse = {sep_mse[t,m,d,c]}\n\n")
 
@@ -124,9 +125,9 @@ print(f"clean_param = {clean_param_best}\n")
 print("Running SEP...\n")
 
 runtime = torch.zeros(num_images)
-counts = torch.zeros(num_images)
-locs = torch.zeros(num_images, max_detections, 2)
-fluxes = torch.zeros(num_images, max_detections)
+unpruned_counts = torch.zeros(num_images)
+unpruned_locs = torch.zeros(num_images, max_detections, 2)
+unpruned_fluxes = torch.zeros(num_images, max_detections)
 
 for i in range(num_images):
     start = time.perf_counter()
@@ -145,37 +146,19 @@ for i in range(num_images):
     end = time.perf_counter()
 
     runtime[i] = end - start
-    counts[i] = len(sep_results)
-    count = counts[i].int().item()
-    locs[i, :count, 0] = (
+    unpruned_counts[i] = len(sep_results)
+    unpruned_count = unpruned_counts[i].int().item()
+    unpruned_locs[i, :unpruned_count, 0] = (
         torch.from_numpy(sep_results["y"]) - padding + 0.5
     )  # match SMC locs convention
-    locs[i, :count, 1] = (
+    unpruned_locs[i, :unpruned_count, 1] = (
         torch.from_numpy(sep_results["x"]) - padding + 0.5
     )  # match SMC locs convention
-    fluxes[i, :count] = torch.from_numpy(sep_results["flux"])
+    unpruned_fluxes[i, :unpruned_count] = torch.from_numpy(sep_results["flux"])
 
 # remove unnecessary trailing zeros
-locs = locs[:, : counts.max().int().item(), :]
-fluxes = fluxes[:, : counts.max().int().item()]
-
-# prune detections a la Aggregate.prune
-in_bounds = torch.all(
-    torch.logical_and(locs > 0, locs < torch.tensor((image_height, image_width))),
-    dim=-1,
-)
-
-counts = in_bounds.sum(-1).float()
-
-locs = in_bounds.unsqueeze(-1) * locs
-locs_mask = (locs != 0).int()
-locs_index = torch.sort(locs_mask, dim=1, descending=True)[1]
-locs = torch.gather(locs, dim=1, index=locs_index)
-
-fluxes = in_bounds * fluxes
-fluxes_mask = (fluxes != 0).int()
-fluxes_index = torch.sort(fluxes_mask, dim=1, descending=True)[1]
-fluxes = torch.gather(fluxes, dim=1, index=fluxes_index)
+unpruned_locs = unpruned_locs[:, : unpruned_counts.max().int().item(), :]
+unpruned_fluxes = unpruned_fluxes[:, : unpruned_counts.max().int().item()]
 ##############################################
 
 ##############################################
@@ -184,9 +167,9 @@ fluxes = torch.gather(fluxes, dim=1, index=fluxes_index)
 print("Saving results...\n")
 
 torch.save(runtime.cpu(), "results/sep/runtime.pt")
-torch.save(counts.cpu(), "results/sep/counts.pt")
-torch.save(locs.cpu(), "results/sep/locs.pt")
-torch.save(fluxes.cpu(), "results/sep/fluxes.pt")
+torch.save(unpruned_counts.cpu(), "results/sep/unpruned_counts.pt")
+torch.save(unpruned_locs.cpu(), "results/sep/unpruned_locs.pt")
+torch.save(unpruned_fluxes.cpu(), "results/sep/unpruned_fluxes.pt")
 
 print("Done!")
 ##############################################
