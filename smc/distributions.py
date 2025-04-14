95fed36c327c3112e897382e49d7644bcfdecbb8
Tim White <twhit@umich.edu>
2025-04-14 14:00:18 -0400

    TruncatedPareto class

--- smc/distributions.py
+++ smc/distributions.py
@@ -39,3 +39,34 @@ class TruncatedDiagonalMVN(Distribution):
         cdf_at_lb = self.base_dist.cdf(self.lb)
         log_cdf = (cdf_at_val - cdf_at_lb + 1e-9).log().sum(-1) - self.log_prob_in_box
         return log_cdf.exp()
+
+
+class TruncatedPareto(Distribution):
+    """See https://en.wikipedia.org/wiki/Pareto_distribution#Bounded_Pareto_distribution."""
+
+    def __init__(self, alpha, lower, upper):
+        self.alpha = torch.tensor(alpha)
+        self.lower = torch.tensor(lower)
+        self.upper = torch.tensor(upper)
+
+        self.logpdf_norm_const = (
+            self.alpha.log()
+            + alpha * self.lower.log()
+            + self.alpha * self.upper.log()
+            - (self.upper**self.alpha - self.lower**self.alpha).log()
+        )
+
+    def sample(self, shape=[]):
+        unif = torch.rand(shape)
+        numerator = (
+            self.upper**self.alpha
+            - unif * (self.upper**self.alpha)
+            + unif * (self.lower**self.alpha)
+        )
+        denominator = (self.lower**self.alpha) * (self.upper**self.alpha)
+        exponent = -1 / self.alpha
+        return (numerator / denominator) ** exponent
+
+    def log_prob(self, value):
+        assert (value >= self.lower).all() and (value <= self.upper).all()
+        return self.logpdf_norm_const - (self.alpha + 1) * value.log()
